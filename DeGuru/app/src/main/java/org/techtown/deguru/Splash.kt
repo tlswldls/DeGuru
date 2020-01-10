39bf16c1cf4eec06b0dc53df5bbf22b92733acd9
tlswldls <42241350+tlswldls@users.noreply.github.com>
2020-01-10 14:38:24 +0900

    Edited Splash
    디폴트로 프로젝트에 포함되어있는 xml파일로 스플래시 화면 구현했습니다.
    drawable에 xml파일 추가해서 styles에 item이름만 바꿔주면 돼요

--- DeGuru/app/src/main/java/org/techtown/deguru/Splash.kt
+++ DeGuru/app/src/main/java/org/techtown/deguru/Splash.kt
@@ -1,12 +1,18 @@
 package org.techtown.deguru
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class Splash : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        var intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
