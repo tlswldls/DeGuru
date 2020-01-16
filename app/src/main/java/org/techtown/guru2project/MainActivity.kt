7117778f8062c6a4f88e935aeb8cc0de92915cdf
tlswldls <42241350+tlswldls@users.noreply.github.com>
2020-01-16 12:18:13 +0900

    create new project
    프로젝트 새로 만들었음

---
+++ app/src/main/java/org/techtown/guru2project/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.techtown.guru2project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
