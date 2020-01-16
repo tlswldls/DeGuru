6318a3bb8125f97524633b99406150799fd48603
tlswldls <42241350+tlswldls@users.noreply.github.com>
2020-01-16 15:10:47 +0900

    Edit PageNavigation
    레이아웃(XML) 업데이트 전 기존의 버튼과 내용으로 구현한 레이아웃입니다.

--- app/src/main/java/org/techtown/guru2project/MenuActivity.kt
+++ app/src/main/java/org/techtown/guru2project/MenuActivity.kt
@@ -1,12 +1,22 @@
 package org.techtown.guru2project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        logoutBtn_menu.setOnClickListener {
+            val intent = intent?:return@setOnClickListener
+            intent.putExtra(MainActivity.RESULT, "logout")
+            setResult(MainActivity.REQUEST_CODE, intent)
+            finish()
+        }
     }
+
 }
