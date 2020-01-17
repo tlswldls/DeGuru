45f3bf9a0bff8d729bb2f3cc864e37958b1d8471
tlswldls <42241350+tlswldls@users.noreply.github.com>
2020-01-18 07:04:38 +0900

    Update Todo.kt
    DB에 넣어줘야 하는 데이터 필드 추가했습니다.

--- app/src/main/java/org/techtown/guru2project/Todo.kt
+++ app/src/main/java/org/techtown/guru2project/Todo.kt
@@ -1,4 +1,4 @@
 package org.techtown.guru2project
 
-data class Todo (val todo: String, val date: String, val place: String, val latitude: String,
-                 val longitude: String, val index: String)
+data class Todo (val todo: String, var date: String, var place: String, var latitude: String,
+                 var longitude: String, var address: String,  var index: String)
